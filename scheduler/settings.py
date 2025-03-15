d622dfbff25e71a2bfa3d6bc82f7f6bb6d4f727f
Brian Zollinhofer <brianzollinhofer@gmail.com>
2025-03-15 11:42:00 -0400

    Fix template filter loading and add rest_framework to installed apps

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'schedule',  # Our scheduling app
+    'rest_framework',  # Django REST framework
 ]
 
 MIDDLEWARE = [
